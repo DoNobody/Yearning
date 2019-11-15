003cb97ad0fa3d04f693522a43fcaebc84a83c86
donobody <123liuhongwei@163.com>
2019-11-15 16:18:48 +0800

    fix init-sql add from-ldap column

--- install/init-sql/install.sql
+++ install/init-sql/install.sql
@@ -24,7 +24,7 @@ BEGIN;
 
 	INSERT INTO `core_globalpermissions`(id,authorization,inception,ldap,message,other,updatetime) VALUES (1, 'global', '{\'host\': \'\', \'port\': \'\', \'user\': \'\', \'password\': \'\', \'back_host\': \'\', \'back_port\': \'\', \'back_user\': \'\', \'back_password\': \'\'}', '{\'type\': \'1\', \'host\': \'\', \'sc\': \'\', \'domain\': \'\', \'user\': \'\', \'password\': \'\'}', '{\'webhook\': \'\', \'smtp_host\': \'\', \'smtp_port\': \'\', \'user\': \'\', \'password\': \'\', \'to_user\': \'\', \'mail\': False, \'ding\': False, \'ssl\': False}', '{\'limit\': \'\', \'con_room\': [\'AWS\', \'Aliyun\', \'Own\', \'Other\'], \'foce\': \'\', \'multi\': False, \'query\': False, \'sensitive_list\': [], \'sensitive\': \'\', \'exclued_db_list\': [], \'exclued_db\': \'\', \'email_suffix_list\': [], \'email_suffix\': \'\'}', NOW());
 
-	INSERT INTO `core_account`(id,`password`,is_superuser, username,first_name,last_name,email, is_staff,is_active,`group`,department,auth_group,date_joined, updatetime) VALUES (1,'pbkdf2_sha256$100000$Dy6mFniGxTZa$YBQ9cX0iPQvTYp06C5ZiVgXICTHNTiwWhWYnRmcqjHY=',0, 'admin','admin','admin','', 1, 1, 'admin','dba','admin', NOW(), NOW());
+	INSERT INTO `core_account`(id,`password`,is_superuser, username,first_name,last_name,email, is_staff,is_active,`group`,department,auth_group,from_ldap,date_joined,updatetime) VALUES (1,'pbkdf2_sha256$100000$Dy6mFniGxTZa$YBQ9cX0iPQvTYp06C5ZiVgXICTHNTiwWhWYnRmcqjHY=',0, 'admin','admin','admin','', 1, 1, 'admin','dba','admin',0,NOW(), NOW());
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
